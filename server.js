4500c1d532fad8eec90c83f7ccee2712d18aa334
gzych <gzych@egnyte.com>
2025-05-20 17:44:47 +0200

    Fix issues

--- server.js
+++ server.js
@@ -229,5 +229,8 @@ app.put('/api/todos/:id', authenticate, (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));">
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
